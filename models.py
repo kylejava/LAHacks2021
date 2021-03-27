a7972fbd21f1b70e0fac8e2069fff4c7dc998b15
Kyle <kyleedwardgjava@gmail.com>
2021-03-26 21:34:10 -0700

    Need to finish backend

--- models.py
+++ models.py
@@ -2,3 +2,25 @@ from keys import *
 import requests
 import json
 from pprint import pprint
+
+
+
+def findBusiness(choice , city):
+    businesses = []
+    ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
+    HEADERS = {'Authorization': 'Bearer %s' % api_key}
+    PARAMETERS = {
+                    'term': ("Local " + choice),
+                    'radius': 10000,
+                    'location': city,
+                    'limit': 10,
+                    'offset': 1,
+
+                    }
+    response = requests.get(url = ENDPOINT , params = PARAMETERS, headers = HEADERS)
+    business_data = response.json()
+    for biz in business_data['businesses']:
+
+    return businesses
+
+findBusiness("Coffee Shops" , "Pasadena")
