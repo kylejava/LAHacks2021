91e18cabfa71eea79dae0ae9f53f3a98c02fce94
Kyle <kyleedwardgjava@gmail.com>
2021-03-27 00:28:54 -0700

    Fixed it

--- main.py
+++ main.py
@@ -10,5 +10,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/result', methods = ['GET', 'POST'])
+def results():
+    return render_template('results.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
